4e8535eaa9dd4e752e5025fed5934c6ab14a084b
Xgl <285293728@qq.com>
2017-04-29 22:27:21 +0800

    Chat.java(Chat thread control on server)
    Make some changes to adapt to the communication of Client

--- Chat.java
+++ Chat.java
@@ -18,7 +18,8 @@ public class Chat extends Thread {
 	public void out(String out) {
 		try {
 			PrintStream ps = new PrintStream(socket.getOutputStream());
-			ps.println(out + "\n");
+			ps.println(out);
+			ps.close();
 
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
@@ -31,19 +32,21 @@ public class Chat extends Thread {
 
 	@Override
 	public void run() {
-		out("You have already connected with Sever. Your name would be User "+Manager.getChatManager().getUserID(this)+".");
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 
 			String line = null;
+			
+			//socket里的流，只有关闭socket才会读到null
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
-				Manager.getChatManager().publish(this, line);
+				Manager.getChatManager().publish(line);
 			}
 
 			br.close();
 			System.out.println("break connection(br.close)");
 			Manager.getChatManager().remove(this);
+			
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
